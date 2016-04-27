912cb4bd2617ddec32a9cf1a91a85bad5c71a0b7
Steven Wang <steven@stevens-macbook-pro.local>
2016-04-27 16:43:12 +0800

    1st

--- GuessNumber/ViewController.swift
+++ GuessNumber/ViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var inputNumberField: UITextField!
+    
+    @IBOutlet weak var showMessageField: UILabel!
+    
+
+    @IBOutlet weak var sumTimesLabel: UILabel!
+    
+    var value2:Int = 0
+    @IBAction func comfirmButton(sender: AnyObject) {
+        let value1 = "5"
+        
+        
+       
+    if value2 < 6 {
+        
+        if let guessNumber = inputNumberField.text {
+            
+            if Int(guessNumber) <= 10 {
+                if Int(guessNumber) > 0 {
+                    if guessNumber == value1 {
+                        showMessageField.text="正確"
+                        value2 = value2 + 1
+                        sumTimesLabel.text="\(value2)"
+                        
+                    }
+                    else {
+                        showMessageField.text="不正確"
+                        value2 = value2 + 1
+                        sumTimesLabel.text="\(value2)"
+                    }
+                }
+                }
+    
+        else {
+            showMessageField.text="輸入範圍1~10"
+        }
+    }
+        
+        
+    }
+     else {
+           showMessageField.text="超過6次"
+        }   
+        
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
